5e57dc4dce8adf8855f349b112846d59ef2ed38b
“lrodriguezcabrales” <lrodriguez@araujoysegovia.com>
2014-07-23 14:21:23 -0500

    html login

--- src/Lider/Bundle/LiderBundle/Controller/RoutingController.php
+++ src/Lider/Bundle/LiderBundle/Controller/RoutingController.php
@@ -4,6 +4,8 @@ namespace Lider\Bundle\LiderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class RoutingController extends Controller
 {
@@ -12,8 +14,22 @@ class RoutingController extends Controller
         return $this->render('LiderBundle:Default:index.html.twig', array('name' => $name));
     }
 
-    public function loginPageAction()
+    /**   
+     * @Template("LiderBundle:Lider:index.html.twig")
+     */
+    public function loginPageAction(Request $request)
     {
-    	return $this->render('LiderBundle:Lider:index.html.twig');
+    	if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        );
+
+    	//return $this->render('LiderBundle:Lider:index.html.twig');
     }
 }
