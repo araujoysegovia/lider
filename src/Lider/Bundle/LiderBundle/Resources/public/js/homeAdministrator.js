a29d7bc246ef7667006596bc0d09721a7c4a309d
“lrodriguezcabrales” <lrodriguez@araujoysegovia.com>
2014-07-25 15:54:37 -0500

    html..

--- src/Lider/Bundle/LiderBundle/Resources/public/js/homeAdministrator.js
+++ src/Lider/Bundle/LiderBundle/Resources/public/js/homeAdministrator.js
@@ -1,55 +1,100 @@
 $(document).ready(function () {
 
-    // var crudServiceBaseUrl = "http://localhost/lider/web/app_dev.php",
-        dataSource = new kendo.data.DataSource({
-            transport: {
-                read:  {
-                    url:  "admin/tournament",
-                    dataType: "json",
-                    type: "GET"
-                },
-                update: {
-                    url: "admin/tournament",
-                    dataType: "json",
-                    type: "PUT"
-                },
-                destroy: {
-                    url: "admin/tournament",
-                    dataType: "json",
-                    type: "DELETE"
-                },
-                create: {
-                    url: "admin/tournament",
-                    dataType: "POST"
-                },
-                parameterMap: function(options, operation) {
-                    if (operation !== "read" && options.models) {
-                        return {models: kendo.stringify(options.models)};
-                    }
-                }
+    $("#linkTournaments").click(function () {
+
+        $(".tournaments").css("display","block");
+        $(".groups").css("display","none");
+        $(".players").css("display","none");
+        $(".categories").css("display","none");
+        $(".questions").css("display","none");
+    });
+
+    $("#linkPlayers").click(function () {
+
+        $(".players").css("display","block");
+        $(".tournaments").css("display","none");
+        $(".groups").css("display","none");
+        $(".categories").css("display","none");
+        $(".questions").css("display","none");
+    });
+
+    $("#linkGroups").click(function () {
+
+        $(".groups").css("display","block");
+        $(".players").css("display","none");
+        $(".tournaments").css("display","none");
+        $(".categories").css("display","none");
+        $(".questions").css("display","none");
+    });        
+    
+    $("#linkCategories").click(function () {
+
+        $(".categories").css("display","block");
+        $(".players").css("display","none");
+        $(".tournaments").css("display","none");
+        $(".groups").css("display","none");
+        $(".questions").css("display","none");
+    });      
+
+    $("#linkQuestions").click(function () {
+
+        $(".questions").css("display","block");
+        $(".categories").css("display","none");
+        $(".players").css("display","none");
+        $(".tournaments").css("display","none");
+        $(".groups").css("display","none");
+
+    });      
+
+    /**************** KENDO GRIDS ******************/
+    
+    dataSourceTournaments = new kendo.data.DataSource({
+        transport: {
+            read:  {
+                url:  "admin/tournament",
+                dataType: "json",
+                type: "GET"
             },
-            batch: true,
-            pageSize: 20,
-            schema: {
-                model: {
-                    id: "id",
-                    fields: {
-                        tournamentId: { editable: false, nullable: true },
-                        name: { validation: { required: true } },
-                        startDate: { type: "date", },
-                        endDate: { type: "date" },
-                        active: { type: "boolean" }
-                    }
+            update: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "PUT"
+            },
+            destroy: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "DELETE"
+            },
+            create: {
+                url: "admin/tournament",
+                dataType: "POST"
+            },
+            parameterMap: function(options, operation) {
+                if (operation !== "read" && options.models) {
+                    return {models: kendo.stringify(options.models)};
                 }
             }
-        });
-
-
+        },
+        batch: true,
+        pageSize: 20,
+        schema: {
+            model: {
+                id: "id",
+                fields: {
+                    tournamentId: { editable: false, nullable: true },
+                    name: { validation: { required: true } },
+                    startDate: { type: "date", },
+                    endDate: { type: "date" },
+                    active: { type: "boolean" }
+                }
+            }
+        }
+    });
 
     $("#gridTournaments").kendoGrid({
-        dataSource: dataSource,
+        dataSource: dataSourceTournaments,
         pageable: true,
-        height: 550,
+        height: 500,
         toolbar: ["create"],
         columns: [
             { field:"name", title: "Nombre" },
@@ -61,4 +106,244 @@ $(document).ready(function () {
     });
 
 
+    dataSourcePlayers = new kendo.data.DataSource({
+        transport: {
+            read:  {
+                url:  "admin/tournament",
+                dataType: "json",
+                type: "GET"
+            },
+            update: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "PUT"
+            },
+            destroy: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "DELETE"
+            },
+            create: {
+                url: "admin/tournament",
+                dataType: "POST"
+            },
+            parameterMap: function(options, operation) {
+                if (operation !== "read" && options.models) {
+                    return {models: kendo.stringify(options.models)};
+                }
+            }
+        },
+        batch: true,
+        pageSize: 20,
+        schema: {
+            model: {
+                id: "id",
+                fields: {
+                    tournamentId: { editable: false, nullable: true },
+                    name: { validation: { required: true } },
+                    startDate: { type: "date", },
+                    endDate: { type: "date" },
+                    active: { type: "boolean" }
+                }
+            }
+        }
+    });
+
+    $("#gridPlayers").kendoGrid({
+        dataSource: dataSourcePlayers,
+        pageable: true,
+        height: 500,
+        toolbar: ["create"],
+        columns: [
+            { field:"name", title: "Nombre" },
+            { field: "lastname", title:"Apellido" },
+            { field: "office_id", title:"Oficina"},
+            { field: "team_id", title: "Equipo"},
+            { command: ["edit", "destroy"], title: "&nbsp;", width: "200px" }],
+        editable: "popup"
+    });
+
+    dataSourceGroups = new kendo.data.DataSource({
+        transport: {
+            read:  {
+                url:  "admin/tournament",
+                dataType: "json",
+                type: "GET"
+            },
+            update: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "PUT"
+            },
+            destroy: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "DELETE"
+            },
+            create: {
+                url: "admin/tournament",
+                dataType: "POST"
+            },
+            parameterMap: function(options, operation) {
+                if (operation !== "read" && options.models) {
+                    return {models: kendo.stringify(options.models)};
+                }
+            }
+        },
+        batch: true,
+        pageSize: 20,
+        schema: {
+            model: {
+                id: "id",
+                fields: {
+                    tournamentId: { editable: false, nullable: true },
+                    name: { validation: { required: true } },
+                    startDate: { type: "date", },
+                    endDate: { type: "date" },
+                    active: { type: "boolean" }
+                }
+            }
+        }
+    });
+
+    $("#gridGroups").kendoGrid({
+        dataSource: dataSourceGroups,
+        pageable: true,
+        height: 500,
+        toolbar: ["create"],
+        columns: [
+            { field:"name", title: "Nombre" },
+            { field:"tournament_id", title: "Toreo" },
+            { field: "entryDate", title:"Fecha de inicio" },
+            { field: "lastUpdate", title:"Ultima modificación"},
+            { field: "active", title: "Activo"},
+            { command: ["edit", "destroy"], title: "&nbsp;", width: "200px" }],
+        editable: "popup"
+    });
+
+
+    dataSourceCategories = new kendo.data.DataSource({
+        transport: {
+            read:  {
+                url:  "admin/tournament",
+                dataType: "json",
+                type: "GET"
+            },
+            update: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "PUT"
+            },
+            destroy: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "DELETE"
+            },
+            create: {
+                url: "admin/tournament",
+                dataType: "POST"
+            },
+            parameterMap: function(options, operation) {
+                if (operation !== "read" && options.models) {
+                    return {models: kendo.stringify(options.models)};
+                }
+            }
+        },
+        batch: true,
+        pageSize: 20,
+        schema: {
+            model: {
+                id: "id",
+                fields: {
+                    tournamentId: { editable: false, nullable: true },
+                    name: { validation: { required: true } },
+                    startDate: { type: "date", },
+                    endDate: { type: "date" },
+                    active: { type: "boolean" }
+                }
+            }
+        }
+    });
+
+    $("#gridCategories").kendoGrid({
+        dataSource: dataSourceCategories,
+        pageable: true,
+        height: 500,
+        toolbar: ["create"],
+        columns: [
+            { field:"name", title: "Nombre" },            
+            { field: "entryDate", title:"Fecha de inicio" },
+            { field: "lastUpdate", title:"Ultima modificación"},            
+            { command: ["edit", "destroy"], title: "&nbsp;", width: "200px" }],
+        editable: "popup"
+    });
+
+
+    dataSourceQuestions = new kendo.data.DataSource({
+        transport: {
+            read:  {
+                url:  "admin/tournament",
+                dataType: "json",
+                type: "GET"
+            },
+            update: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "PUT"
+            },
+            destroy: {
+                url: "admin/tournament",
+                dataType: "json",
+                type: "DELETE"
+            },
+            create: {
+                url: "admin/tournament",
+                dataType: "POST"
+            },
+            parameterMap: function(options, operation) {
+                if (operation !== "read" && options.models) {
+                    return {models: kendo.stringify(options.models)};
+                }
+            }
+        },
+        batch: true,
+        pageSize: 20,
+        schema: {
+            model: {
+                id: "id",
+                fields: {
+                    tournamentId: { editable: false, nullable: true },
+                    category: { defaultValue: { CategoryID: 1, CategoryName: ""} },
+                }
+            }
+        }
+    });
+
+    $("#gridQuestions").kendoGrid({
+        dataSource: dataSourceQuestions,
+        pageable: true,
+        height: 500,
+        toolbar: ["create"],
+        columns: [
+            { field:"question", title: "Pregunta" },            
+            //{ field: "category_id", title:"Categoria" }, 
+            { field: "category", title: "Category", width: "180px", 
+            editor: categoryDropDownEditor, template: "#=category.CategoryName#" },             
+            { command: ["edit", "destroy"], title: "&nbsp;", width: "200px" }],
+        editable: "popup"
+    });
+
+    function categoryDropDownEditor(container, options) {
+    $('<input required data-text-field="CategoryName" data-value-field="CategoryID" data-bind="value:' + options.field + '"/>')
+        .appendTo(container)
+        .kendoDropDownList({
+            autoBind: false,
+            dataSource: {
+                type: "odata",
+                transport: {
+                    read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Categories"
+                }
+            }
+        });
+    }    
 });
--- src/Lider/Bundle/LiderBundle/Resources/public/js/login.js
+++ src/Lider/Bundle/LiderBundle/Resources/public/js/login.js
@@ -1,47 +1,61 @@
 $(document).ready(function () {
 
-	formData = new FormData();
-	formData.append(name, value);
+	
+
+	
+	
+	var form = $("form[role=form]");
 
 	form.submit(function(e){
-	
-	alert("dadad")
-	e.preventDefault();
-
-	$.ajax({
-		type: "POST",     
-		url: "",
-        data: formData,
-        contentType: false,
-        processData: false,
-
-		config.success = function(obj, textStatus, jqXHR){
-			if(obj["message"]){
-		    	notification.show({
-	                title: "Success",
-                    message: obj.message
-                }, "success");
-			}
-		},
 		
-		config.error = function(xhr, textStatus, jqXHR){
-			try{
-		    	var obj = jQuery.parseJSON(xhr.responseText);
-		    	notification.show({
-                    title: "Error",
-                    message: obj.message
-                }, "error");
-	    	}catch(ex){
-	    		notification.show({
-                    title: "Error",
-                    message: "Server error"
-                }, "error");
-	    	}
+		e.preventDefault();
 
-	    	if(xhr.status == 401){
-				  window.location= "/index";
+		formData = new FormData();
+
+ 		var name = form.find("#email").val();
+ 		var password = form.find("#password").val();
+
+
+		formData.append("_username", name);
+		formData.append("_password", password);
+		
+
+		$.ajax({
+			type: "POST",     
+			url: "admin/login",
+	        data: formData,
+	        contentType: false,
+	        processData: false,
+
+			success : function(obj, textStatus, jqXHR){
+				if(obj["message"]){
+			    	notification.show({
+		                title: "Success",
+	                    message: obj.message
+	                }, "success");
+				}
+			},
+			
+			error : function(xhr, textStatus, jqXHR){
+				try{
+			    	var obj = jQuery.parseJSON(xhr.responseText);
+			    	notification.show({
+	                    title: "Error",
+	                    message: obj.message
+	                }, "error");
+		    	}catch(ex){
+		    		notification.show({
+	                    title: "Error",
+	                    message: "Server error"
+	                }, "error");
+		    	}
+
+		    	if(xhr.status == 401){
+					  window.location= "/index";
+				}
+
 			}
-		});
 
-	});
-}
+		});
+	})
+})
