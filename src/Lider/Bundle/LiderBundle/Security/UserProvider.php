335db9e741d3f0036fe204a4fbb4a7c63d585e86
Ing. Deiner Mejia M <root@MacBook-Pro-de-Deiner-2.local>
2014-09-04 14:31:26 -0500

    update changes

--- src/Lider/Bundle/LiderBundle/Security/UserProvider.php
+++ src/Lider/Bundle/LiderBundle/Security/UserProvider.php
@@ -10,7 +10,7 @@ use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 
-class UserProvider extends OAuthUserProvider //implements UserProviderInterface, OAuthAwareUserProviderInterface
+class UserProvider implements UserProviderInterface //implements UserProviderInterface, OAuthAwareUserProviderInterface
 {
 	private $em;
 	private $cont;
@@ -50,20 +50,5 @@ class UserProvider extends OAuthUserProvider //implements UserProviderInterface,
     public function supportsClass($class)
     {
         return $class === 'Lider\Bundle\LiderBundle\Entity\Player';
-    }
-    
-    public function loadUserByOAuthUserResponse(UserResponseInterface $response) {
-    	$username = $response->getUsername(); /* An ID like: 112259658235204980084 */
-    	$email = $response->getEmail();
-    	$nickname = $response->getNickname();
-    	$realname = $response->getRealName();
-    	$avatar = $response->getProfilePicture();
-    	
-    	$user = $this->em->getRepository("LiderBundle:Player")->findOneByEmail($email);
-    	if(!$user)
-    		throw new UsernameNotFoundException(
-    				sprintf('Username "%s" does not exist.', $username)
-    		);
-    	return $this->loadUserByUsername($response->getUsername());
     }
 }
