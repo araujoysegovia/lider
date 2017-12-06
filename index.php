838b95618c7cfa0da94e5eb0e060e3083c7d93fd
lrodriguez <lrodriguez@araujoysegovia.com>
2017-12-06 17:11:35 +0000

    index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 //header ("Location: http://lider.sifinca.net/lider/web/app.php/admin/");
 
 //header ("Location: http://localhost:8080/lider/web/app_dev.php/admin/");
-header ("Location: https://www.sifinca.net/lider/web/app_dev.php/admin/");
+header ("Location: http://www.sifinca.net/lider/web/app_dev.php/admin/");
 
 ?>
 
